3b2713f654ca59a4b7461aa450b6d885f597c1bd
jose.moura <jose.moura@CEDRONOC-013.cedrofinances.com.br>
2020-01-13 17:07:32 -0300

    Adicionado ativos tipo NASDAQ e NYSE.

--- Crawler/src/WebCrawler.java
+++ Crawler/src/WebCrawler.java
@@ -7,11 +7,11 @@ import org.jsoup.nodes.Document;
 
 public class WebCrawler {
 
-	private static void visitaSite(String ativo) {
+	private static void visitaSite(String ativo, String tipo, String tipoArq, String tipoUrl) {
 		String pagina;
 		String url;
 
-		url = "https://br.tradingview.com/symbols/BMFBOVESPA-" + ativo;
+		url = "https://" + tipoUrl + ".tradingview.com/symbols/" + tipo + ativo;
 
 		try {
 
@@ -25,7 +25,7 @@ public class WebCrawler {
 			pagina = codigo.html();
 
 //			faz o "parse"
-			parse(ativo, pagina);
+			parse(ativo, pagina, tipoArq);
 
 		} catch (Exception err) {
 			System.err.println("Exception err 1.0, A URL não existe.");
@@ -45,10 +45,9 @@ public class WebCrawler {
 		} catch (Exception e) {
 			System.err.println("Exception err");
 		}
-
 	}
 
-	private static void parse(String ativo, String pagina) {
+	private static void parse(String ativo, String pagina, String tipoArq) {
 		int POS_INI = pagina.indexOf("<div class=\"tv-widget-description__text\">");
 		int POS_FIN = pagina.indexOf("</div>", pagina.indexOf("<div class=\"tv-widget-description__text\">"));
 
@@ -57,37 +56,45 @@ public class WebCrawler {
 		String resultado = pagina.substring(POS_INI + 58, POS_FIN);
 
 		if (pagina.contains("<div class=\"tv-widget-description__text\">")) {
-			createFile(resultado, ativo);
+			createFile(resultado, ativo, tipoArq);
 		} else {
 			System.out.println("Não existe, arquivo não criado.");
 		}
-
 		WriteLog("Ativo finalizado: " + ativo);
 	}
 
-	public static void createFile(String resultado, String ativo) {
+	public static void createFile(String resultado, String ativo, String tipoArq) {
 		FileWriter arquivo;
 
 		try {
-			arquivo = new FileWriter(new File("C:\\Ativos\\" + ativo.substring(0,4) + ".txt"));
+//			tratamento para ativos com apenas 3 letras EX: IBM
+			if (ativo.length() < 3) {
+				arquivo = new FileWriter(new File("C:\\Ativos\\" + tipoArq + "\\" + ativo.substring(0, 2) + ".txt"));
+			} else if (ativo.length() < 4) {
+				arquivo = new FileWriter(new File("C:\\Ativos\\" + tipoArq + "\\" + ativo.substring(0, 3) + ".txt"));
+			} else {
+				arquivo = new FileWriter(new File("C:\\Ativos\\" + tipoArq + "\\" + ativo.substring(0, 4) + ".txt"));
+			}
 			arquivo.write(resultado);
 			arquivo.close();
-
 		} catch (IOException e) {
 			System.err.println("IO Exception err");
 		} catch (Exception e) {
 			System.err.println("Exception err");
 		}
-
 	}
 
 	public static void main(String args[]) {
 
 		List<String> ListaAtivos = new ArrayList<String>();
+		List<String> ListaAtivos1 = new ArrayList<String>();
+		List<String> ListaAtivos2 = new ArrayList<String>();
+
 		long tempoInicio = System.currentTimeMillis();
 
+//		Ativos Bovespa ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		try {
-			BufferedReader listaTxt = new BufferedReader(new FileReader("C:\\Ativos\\ListaAtivos.txt"));
+			BufferedReader listaTxt = new BufferedReader(new FileReader("C:\\Ativos\\ListaAtivos\\ListaAtivosBovespa.txt"));
 
 			while (listaTxt.ready()) {
 				ListaAtivos.add(listaTxt.readLine());
@@ -100,7 +107,45 @@ public class WebCrawler {
 		}
 
 		for (int i = 0; i < ListaAtivos.size(); i++) {
-			visitaSite(ListaAtivos.get(i));
+			visitaSite(ListaAtivos.get(i), "BMFBOVESPA-", "AtivosBovespa", "br");
+		}
+
+//		Ativos Nasdaq ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		try {
+			BufferedReader listaTxt1 = new BufferedReader(new FileReader("C:\\Ativos\\ListaAtivos\\ListaAtivosNasdaq.txt"));
+
+			while (listaTxt1.ready()) {
+				ListaAtivos1.add(listaTxt1.readLine());
+			}
+
+			listaTxt1.close();
+
+		} catch (Exception err) {
+			System.err.println("Exception 3.0");
+		}
+//		AtivosNasdaqBR && AtivosNasdaqUS
+		for (int i = 0; i < ListaAtivos1.size(); i++) {
+			visitaSite(ListaAtivos1.get(i), "NASDAQ-", "AtivosNasdaqBR", "br");
+			visitaSite(ListaAtivos1.get(i), "NASDAQ-", "AtivosNasdaqUS", "in");
+		}
+
+//		Ativos Nyse ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		try {
+			BufferedReader listaTxt2 = new BufferedReader(new FileReader("C:\\Ativos\\ListaAtivos\\ListaAtivosNyse.txt"));
+
+			while (listaTxt2.ready()) {
+				ListaAtivos2.add(listaTxt2.readLine());
+			}
+
+			listaTxt2.close();
+
+		} catch (Exception err) {
+			System.err.println("Exception 4.0");
+		}
+//		AtivosNyseBR && AtivosNyseUS
+		for (int i = 0; i < ListaAtivos2.size(); i++) {
+			visitaSite(ListaAtivos2.get(i), "NYSE-", "AtivosNyseBR", "br");
+			visitaSite(ListaAtivos2.get(i), "NYSE-", "AtivosNyseUS", "in");
 		}
 
 		long tempoFinal = System.currentTimeMillis();
